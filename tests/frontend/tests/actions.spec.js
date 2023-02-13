fe78f1e1c769b6586e918c6632ef09df58ab8d60
nivrichter <nivrichter@gmail.com>
2023-02-13 16:46:42 +0200

    changed to env vars

--- tests/frontend/tests/actions.spec.js
+++ tests/frontend/tests/actions.spec.js
@@ -7,11 +7,10 @@ const username = `user${(Math.random() + 1).toString(36).substring(7)}`;
 const email = `${username}@email.com`
 const password = `pass${(Math.random() + 1).toString(36).substring(7)}`;
 
-
 const expectCreateUserRequest = (page,  username, email, password ) => {
     const requestId = uid()
     page.on('request', (request) => {
-        if(request.url() === 'http://localhost:3000/api/users' && request.method() === 'POST') {
+        if(request.url() === `${process.env.BACKEND_API_URL}/users` && request.method() === 'POST') {
             expect(JSON.parse(request.postData())?.user?.username).toEqual(username)
             expect(JSON.parse(request.postData())?.user?.email).toEqual(email)
             expect(JSON.parse(request.postData())?.user?.password).toEqual(password)
@@ -24,7 +23,7 @@ const expectCreateUserRequest = (page,  username, email, password ) => {
 const expectCreateItemRequest = (page,  title, description, image ) => {
     const requestId = uid()
     page.on('request', (request) => {
-        if(request.url() === 'http://localhost:3000/api/items' && request.method() === 'POST') {
+        if(request.url() === `${process.env.BACKEND_API_URL}/items`  && request.method() === 'POST') {
             expect(JSON.parse(request.postData())?.item?.title).toEqual(title)
             expect(JSON.parse(request.postData())?.item?.description).toEqual(description)
             expect(JSON.parse(request.postData())?.item?.image).toEqual(image)
@@ -35,7 +34,7 @@ const expectCreateItemRequest = (page,  title, description, image ) => {
 }
 
 test('Creates a user', async ({page}) => {
-    await page.goto('http://localhost:3001/');
+    await page.goto(`${process.env.REACT_APP_URL}`);
     await page.getByRole('link', {name: 'Sign up'}).click();
     await page.getByPlaceholder('Username').fill(username);
     await page.getByPlaceholder('Password').fill(password);
@@ -48,7 +47,7 @@ test('Creates an item', async ({page}) => {
     const title = "title"
     const description = "description"
     const imageUrl = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-    await page.goto('http://localhost:3001/editor');
+    await page.goto(`${process.env.REACT_APP_URL}/editor`);
     await page.getByPlaceholder('Item Title').fill(title);
     await page.getByPlaceholder('What\'s this item about?').fill(description);
     await page.getByPlaceholder('Image url').fill(imageUrl);
