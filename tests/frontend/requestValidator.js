2318d16a1707bd71c20e416fb5199a78af0d0a73
nivrichter <nivrichter@gmail.com>
2023-02-15 16:24:04 +0200

    removed all busy wait

--- tests/frontend/requestValidator.js
+++ tests/frontend/requestValidator.js
@@ -49,36 +49,6 @@ const execAndWaitForRequest = async (eventId, func, maxTime = 500) => {
     unsubscribe(eventId, eventCallback);
   }
 };
-//
-// const execAndWaitForRequest = async (eventId, func, maxTime = 500) => {
-//   let eventReceived = false;
-//   const eventCallback = () => {
-//     eventReceived = true;
-//   };
-//
-//   subscribe(eventId, eventCallback);
-//
-//   await func();
-//
-//   const start = Date.now();
-//
-//   while (Date.now() - start < maxTime) {
-//     if (eventReceived) {
-//       console.log(`The event ${eventId} was sent to Wilco`);
-//       break;
-//     }
-//
-//     await sleep(100);
-//   }
-//
-//   unsubscribe(eventId, eventCallback);
-//
-//   try {
-//     expect(eventReceived).toBe(true);
-//   } catch {
-//     throw new Error(`The event ${eventId} was not sent to Wilco`);
-//   }
-// };
 
 module.exports = {
   dispatch,
