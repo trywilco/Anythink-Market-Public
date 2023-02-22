dce7ddb326b667e265e797e1b919ed3f7ac38d1c
nivrichter <nivrichter@gmail.com>
2023-02-22 09:14:52 +0200

    swapped ports

--- tests/frontend/global-setup.js
+++ tests/frontend/global-setup.js
@@ -1,4 +1,4 @@
 module.exports = async (config) => {
-  process.env.REACT_APP_URL = "http://localhost:3001";
-  process.env.BACKEND_API_URL = "http://localhost:3000/api";
+  process.env.REACT_APP_URL = "http://localhost:3000";
+  process.env.BACKEND_API_URL = "http://localhost:3001/api";
 };
