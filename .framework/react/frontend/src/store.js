93f723bc37723950d47cc1071bdfc99b68614622
Shem <smagnezi8@gmail.com>
2023-03-19 11:30:36 +0000

    remove redux-logger

--- .framework/react/frontend/src/store.js
+++ .framework/react/frontend/src/store.js
@@ -1,5 +1,4 @@
 import { applyMiddleware, createStore } from "redux";
-import { createLogger } from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import { promiseMiddleware, localStorageMiddleware } from "./middleware";
 import reducer from "./reducer";
@@ -25,7 +24,6 @@ const getMiddleware = () => {
       myRouterMiddleware,
       promiseMiddleware,
       localStorageMiddleware,
-      createLogger()
     );
   }
 };
