314f81fb11b17f144d059358277320f94147b972
NivRichter <nivrichter@gmail.com>
2024-02-08 14:47:50 +0200

    fix env of setup script (#48)
    * fix env of setup script
    
    * removed env from cmd

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,7 @@
 WILCO_ID="`cat .wilco`"
-ENGINE_EVENT_ENDPOINT="${ENGINE_BASE_URL}/users/${WILCO_ID}/event"
 CODESPACE_BACKEND_HOST=$(curl -s "${ENGINE_BASE_URL}/api/v1/codespace/backendHost?codespaceName=${CODESPACE_NAME}&portForwarding=${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}" | jq -r '.codespaceBackendHost')
 CODESPACE_BACKEND_URL="https://${CODESPACE_BACKEND_HOST}"
+export ENGINE_EVENT_ENDPOINT="${ENGINE_BASE_URL}/users/${WILCO_ID}/event"
 
 # Update engine that codespace started for user
 curl -L -X POST "${ENGINE_EVENT_ENDPOINT}" -H "Content-Type: application/json" --data-raw "{ \"event\": \"github_codespace_started\" }"
@@ -10,11 +10,9 @@ curl -L -X POST "${ENGINE_EVENT_ENDPOINT}" -H "Content-Type: application/json" -
 echo "export CODESPACE_BACKEND_HOST=\"${CODESPACE_BACKEND_HOST}\"" >> ~/.bashrc
 echo "export CODESPACE_BACKEND_URL=\"${CODESPACE_BACKEND_URL}\"" >> ~/.bashrc
 echo "export CODESPACE_WDS_SOCKET_PORT=443" >> ~/.bashrc
-echo "export ENGINE_EVENT_ENDPOINT=\"${ENGINE_EVENT_ENDPOINT}\"" >> ~/.bashrc
-
 
 # Export welcome prompt in bash:
 echo "printf \"\n\n☁️☁️☁️️ Anythink: Develop in the Cloud ☁️☁️☁️\n\"" >> ~/.bashrc
 echo "printf \"\n\x1b[31m \x1b[1m👉 Type: \\\`docker compose up\\\` to run the project. 👈\n\n\"" >> ~/.bashrc
 
-nohup bash -c 'cd /wilco-agent && node agent.js &' > /dev/null 2>&1  
+nohup bash -c "cd /wilco-agent && node agent.js &" > /dev/null 2>&1  
